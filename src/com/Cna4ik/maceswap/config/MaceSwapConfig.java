f7f44e7bc396cacaf1fec2562e9807e081d844f1
le0derlerwtf-cmyk <le0derlerwtf@gmail.com>
2026-06-06 14:14:03 +0300

    Refactor config: Remove unnecessary features (trident, elytra, refill, shield penetration) and keep only core mace swap with new features

--- src/com/Cna4ik/maceswap/config/MaceSwapConfig.java
+++ src/com/Cna4ik/maceswap/config/MaceSwapConfig.java
@@ -25,79 +25,28 @@ implements ConfigData {
     public boolean hasShownDiscordPrompt = false;
     @ConfigEntry.Category(value="general")
     @ConfigEntry.Gui.EnumHandler(option=ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
-    public DisplayMode displayMode = DisplayMode.ACTIONBAR;
+    public DisplayMode displayMode = DisplayMode.LYRICA_CLIENT;
     @ConfigEntry.Category(value="general")
     @ConfigEntry.Gui.EnumHandler(option=ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
     public SwapItemsMode swapItemsMode = SwapItemsMode.EVERYTHING;
-    @ConfigEntry.Category(value="general")
+    @ConfigEntry.Category(value="swap")
     @ConfigEntry.Gui.Tooltip
-    public boolean autoRefillEnabled = false;
-    @ConfigEntry.Category(value="aim")
+    public boolean stunSlamEnabled = true;
+    @ConfigEntry.Category(value="swap")
     @ConfigEntry.Gui.Tooltip
-    @ConfigEntry.BoundedDiscrete(min=1L, max=50L)
-    public int aimSpeed = 15;
-    @ConfigEntry.Category(value="aim")
+    public boolean autoReturnToAxeEnabled = false;
+    @ConfigEntry.Category(value="hud")
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.BoundedDiscrete(min=1L, max=100L)
-    public int elytraAimSpeed = 40;
-    @ConfigEntry.Category(value="aim")
+    public int saturationKeyBinding = 1;
+    @ConfigEntry.Category(value="hud")
     @ConfigEntry.Gui.Tooltip
-    @ConfigEntry.BoundedDiscrete(min=1L, max=50L)
-    public int aimDistance = 5;
-    @ConfigEntry.Category(value="aim")
-    @ConfigEntry.Gui.Tooltip
-    public boolean playerOnlyAim = true;
-    @ConfigEntry.Category(value="aim")
-    @ConfigEntry.Gui.Tooltip
-    public boolean smartLockEnabled = false;
-    @ConfigEntry.Category(value="combos")
-    @ConfigEntry.Gui.Tooltip
-    public boolean axeSwapEnabled = false;
-    @ConfigEntry.Category(value="combos")
-    @ConfigEntry.Gui.Tooltip
-    public boolean allowAxeSwapHotkey = true;
-    @ConfigEntry.Category(value="combos")
-    @ConfigEntry.Gui.Tooltip
-    public boolean bypassShieldEnabled = false;
-    @ConfigEntry.Category(value="combos")
-    @ConfigEntry.Gui.Tooltip
-    public boolean elytraLaunchEnabled = false;
-    @ConfigEntry.Category(value="combos")
-    @ConfigEntry.Gui.Tooltip
-    public boolean elytraAutoHitEnabled = false;
-    @ConfigEntry.Category(value="combos")
-    @ConfigEntry.Gui.Tooltip
-    public boolean elytraWindBurstChain = false;
-    @ConfigEntry.Category(value="combos")
-    @ConfigEntry.Gui.Tooltip
-    public boolean tridentMaceComboEnabled = false;
-    @ConfigEntry.Category(value="combos")
-    @ConfigEntry.Gui.Tooltip
-    public boolean tridentAutoHitEnabled = false;
-    @ConfigEntry.Category(value="combos")
-    @ConfigEntry.Gui.Tooltip
-    public boolean tridentWindBurstChain = false;
-    @ConfigEntry.Category(value="misc")
-    @ConfigEntry.Gui.Tooltip
-    public boolean autoWindChargeEnabled = false;
-    @ConfigEntry.Category(value="misc")
-    @ConfigEntry.Gui.EnumHandler(option=ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
-    public AutoWindChargeMode autoWindChargeMode = AutoWindChargeMode.ON_LOCK;
-    @ConfigEntry.Category(value="misc")
-    @ConfigEntry.Gui.Tooltip
-    @ConfigEntry.BoundedDiscrete(min=1L, max=10L)
-    public int windFallDistance = 2;
-    @ConfigEntry.Category(value="misc")
-    @ConfigEntry.Gui.Tooltip
-    public boolean autoMlgEnabled = false;
-    @ConfigEntry.Category(value="misc")
-    @ConfigEntry.Gui.Tooltip
-    @ConfigEntry.BoundedDiscrete(min=5L, max=50L)
-    public int mlgFallThreshold = 10;
+    @ConfigEntry.BoundedDiscrete(min=1L, max=100L)
+    public int barKeyBinding = 2;
 
     public static enum DisplayMode {
-        ACTIONBAR("text.autoconfig.maceswap.option.displayMode.ACTIONBAR"),
-        EXCLAMATION_MARK("text.autoconfig.maceswap.option.displayMode.EXCLAMATION_MARK");
+        LYRICA_CLIENT("LyricaClient"),
+        EXCLAMATION_MARK("Exclamation Mark");
 
         private final String key;
 
@@ -111,8 +60,8 @@ implements ConfigData {
     }
 
     public static enum SwapItemsMode {
-        ONLY_WEAPONS("text.autoconfig.maceswap.option.swapItemsMode.ONLY_WEAPONS"),
-        EVERYTHING("text.autoconfig.maceswap.option.swapItemsMode.EVERYTHING");
+        ONLY_WEAPONS("Only Weapons"),
+        EVERYTHING("Everything");
 
         private final String key;
 
@@ -124,20 +73,4 @@ implements ConfigData {
             return this.key;
         }
     }
-
-    public static enum AutoWindChargeMode {
-        ON_LOCK("text.autoconfig.maceswap.option.autoWindChargeMode.ON_LOCK"),
-        ALWAYS("text.autoconfig.maceswap.option.autoWindChargeMode.ALWAYS");
-
-        private final String key;
-
-        private AutoWindChargeMode(String key) {
-            this.key = key;
-        }
-
-        public String toString() {
-            return this.key;
-        }
-    }
 }
-
